c649072dea3d820c1863706d437ae15cb9be9df0
IanRoche <97619360+IanRoche@users.noreply.github.com>
2023-10-30 21:08:15 +0100

    w

--- src/Enemies/Enemies.lua
+++ src/Enemies/Enemies.lua
@@ -1,11 +1,16 @@
 local Object = Object or require "lib.classic"
 local ICanBeDestroyedInterface = require "src.ICabBeDestroyed"
+local Allahakbar=require "src.Enemies.Enemy_AllahAkbar"
+local Enemies=Object:extend()
 
 
 function Enemies:new()
+    Allahakbar:new()
 end
+
 function Enemies:update(dt)
 end
+
 function Enemies:draw()
 end
 
