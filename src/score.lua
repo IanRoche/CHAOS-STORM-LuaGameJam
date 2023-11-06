98b0f9ec81eacec76ba8232f04fcd6d92f40710c
Grazielly Sanchez <gsanchezn@edu.tecnocampus.cat>
2023-11-06 20:40:32 +0100

    Tenemos tutorialPRIMITIVO
    ahora lo pondre bonito

--- src/score.lua
+++ src/score.lua
@@ -1,11 +1,14 @@
 local Object = Object or require "lib.classic"
 local Score = Object:extend()
+local tutorialTime
+
 
 function Score:new()
     self.score = _Score  -- Inicializa la puntuación a cero
     self.font = love.graphics.newFont(24)  -- Fuente para la puntuación
     self.timeElapsed = 0  -- Lleva un seguimiento del tiempo transcurrido
     self.pointsPerSecond = 1  -- Puntos por segundo
+    tutorialTime = true
 end
 
 function Score:update(dt)
@@ -17,6 +20,10 @@ function Score:update(dt)
         self.timeElapsed = self.timeElapsed - 1 
         print(_Score) -- Reinicia el tiempo transcurrido
     end
+    if  _Score >5 and tutorialTime==true then
+        tutorialTime=false
+        _Score=0
+    end
 end
 
 function Score:increaseScore(points)
@@ -27,9 +34,20 @@ function Score:draw()
     -- Establece la fuente para la puntuación
     love.graphics.setFont(self.font)
 
-    -- Dibuja la puntuación en la esquina superior izquierda
+    
+
+    if  tutorialTime then
+
     love.graphics.setColor(1, 1, 1)
-    love.graphics.print("Puntuación: " .. _Score, 10, 10)
+    love.graphics.print("Tutorial", 30,10)
+
+    else   
+        -- Dibuja la puntuación en la esquina superior izquierda
+        love.graphics.setColor(1, 1, 1)
+        
+        love.graphics.print("Puntuación: " .. _Score, 10, 10)
+        end
+
 end
 function Score:GetScore()
     
