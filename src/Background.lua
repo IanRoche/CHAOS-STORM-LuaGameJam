5b95a1a94bf43aa48538290404b974b518f8de2e
Grazielly Sanchez <gsanchezn@edu.tecnocampus.cat>
2023-11-08 14:09:49 +0100

    Lo más cutre que vais a ver hoy
    (no se asusten es muy primitivo todo aun

---
+++ src/Background.lua
@@ -0,0 +1,23 @@
+local Object = Object or require "lib.classic"
+local Background = Object:extend()
+
+function Background:new()
+    self.bgImage = love.graphics.newImage("src/Textures/Fondo.png")
+    self.scrollSpeed = 50  -- velocidad de desplazamiento
+    self.y = 0  -- Posición inicial en el eje Y
+end
+
+function Background:update(dt)
+    self.y = self.y + self.scrollSpeed * dt
+
+    if self.y >= self.bgImage:getHeight() then
+        self.y = 0
+    end
+end
+
+function Background:draw()
+    love.graphics.draw(self.bgImage, 0, self.y)
+    love.graphics.draw(self.bgImage, 0, self.y - self.bgImage:getHeight())
+end
+
+return Background
