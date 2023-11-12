edbd4c21100fb4d0a6b93b0b62da380052fc4855
Paula Ruiz Pareta <pruizpa@edu.tecnocampus.cat>
2023-11-12 11:59:38 +0100

    meme pt.2 juju

--- src/Scenes/GameOver.lua
+++ src/Scenes/GameOver.lua
@@ -28,7 +28,7 @@ function Scene:drawElements()
     -- Dibujar elementos
     love.graphics.setColor(GameOverColor)
     self:drawText("GAME OVER", self.m_gameOverFont, -GameOverFontSize)
-    self:drawText("Puntuación: " .. _Score, self.m_menuFont, 0)
+    self:drawText("Puntuation: " .. _Score, self.m_menuFont, 0)
     self:drawText("¡Si superas el TUTORIAL, vótanos! :)", self.m_menuFont, love.graphics.getHeight() / 3)
     self:drawText("Press ESC to restart", self.m_gameoverEscFont, love.graphics.getHeight() / 2.5)
     love.graphics.setColor(1, 1, 1)
@@ -38,14 +38,19 @@ function Scene:AssignMeme()
     
     if _Score >= ScoreToLevel5 then
         self:drawImage("ener")
+        self:drawImageEasterEgg("pepe", 0.05)
     elseif _Score >= ScoreToLevel4 then
         self:drawImage("kid")
+        self:drawImageEasterEgg("bingus", 0.1)
     elseif _Score >= ScoreToLevel3 then
         self:drawImage("tecnocampus")
+        self:drawImageEasterEgg("belen", 0.4)
     elseif _Score >= ScoreToLevel2 then
         self:drawImage("krater")
+        self:drawImageEasterEgg("mondongo", 0.6)
     else
         self:drawImage("JaumeTeEstimu")
+        self:drawImageEasterEgg("SHREK", 0.1)
     end
 end
 
@@ -61,6 +66,14 @@ function Scene:drawImage(imagePath)
     local x = love.graphics.getWidth() / 2 - image:getWidth() / 2
     local y = love.graphics.getHeight() / 2 - image:getHeight() / 2
     love.graphics.draw(image, x, y)
+end
+
+function Scene:drawImageEasterEgg(imagePath, scale)
+    local image = love.graphics.newImage("src/Textures/" .. imagePath .. ".jpg")
+    local x = 0
+    local y = 0
+    local escala = scale or 0.5
+    love.graphics.draw(image, x, y, 0, escala, escala)
 end
 
 return Scene
